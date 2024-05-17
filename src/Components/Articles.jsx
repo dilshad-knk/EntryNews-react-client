1d587f02b0f57a643de7c0d7e0610bc6c15bc807
Mohamed DIlshad <dilshadali.knk@gmail.com>
2024-05-17 19:24:59 +0530

    image path rectifyied for deployement

--- src/Components/Articles.jsx
+++ src/Components/Articles.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Article from "./Article";
 
 const Articles = () => {
+
   return(
   <section className="grid md:grid-cols-2 lg:grid-cols-3 gap-3  my-4 ">
-    <Article id="01" title="Reviving Retro PCs" description="What happens when old PCs are given modern upgrades?" img="src/assets/images/image-retro-pcs.jpg" />
-    <Article id="02" title="Top 10 Laptops of 2022" description="Our best picks for various needs and budgets." img="src/assets/images/image-top-laptops.jpg" />
-    <Article id="03" title="The Growth of Gaming" description="How the pandemic has sparked fresh opportunities." img="src/assets/images/image-gaming-growth.jpg" />
+    <Article id="01" title="Reviving Retro PCs" description="What happens when old PCs are given modern upgrades?" img="../assets/images/image-retro-pcs.jpg" />
+    <Article id="02" title="Top 10 Laptops of 2022" description="Our best picks for various needs and budgets." img="../assets/images/image-top-laptops.jpg" />
+    <Article id="03" title="The Growth of Gaming" description="How the pandemic has sparked fresh opportunities." img="../assets/images/image-gaming-growth.jpg" />
   </section>
   )
 };
